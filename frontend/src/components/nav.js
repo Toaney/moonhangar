c0be9f4de922ee17178af28416ecd112077d3b5b
Maj Toan <toaney@gmail.com>
2019-12-06 18:35:32 -0800

    update nav

--- frontend/src/components/nav.js
+++ frontend/src/components/nav.js
@@ -6,7 +6,8 @@ const Nav = () => {
         <div className="nav">
             <div className="nav-left">
                 <span className="nav-logo"><Link to="/">Moonhangar</Link></span>
-                <span className="nav-link"><Link to="/home">homepage</Link></span>
+                <span className="nav-link"><Link to="/home">tab1</Link></span>
+                <span className="nav-link"><Link to="/home">tab2</Link></span>
             </div>
             <div className="nav-right">
                 <span className="nav-item"></span>
